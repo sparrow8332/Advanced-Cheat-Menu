5dfae03d1c95bab9e77d4ae5b9b175bbc4659528
sparrow8332 <keith8332@gmail.com>
2019-02-02 16:38:42 +0000

    Added Small menu
    Added Small Menu to Worldtracker
    Cleaned up Functions
    Added Better way to hook Minimap Button
    Fixed Raze City issue with not being able to raze your own cities
    Can Now Raze Captured Capitals (you will loose that Capital in the Domination Race)
    Fixed Error when NO unit is selected and Menu Button Pressed
    Fixed Error when NO City is selected and Menu Button Pressed
    Added WorldTracker Mini Menu (Common Buttons)

---
+++ UI/Panels/Cheat_Menu_Panel_Functions.lua
@@ -0,0 +1,264 @@
+-- // ----------------------------------------------------------------------------------------------
+-- // Author: Sparrow
+-- // DateCreated: 01/24/2019 2:27:04 PM
+-- // ----------------------------------------------------------------------------------------------
+include("Civ6Common");
+include("InstanceManager");
+include("SupportFunctions");
+include("PopupDialog");
+include("AnimSidePanelSupport");
+
+-- // ----------------------------------------------------------------------------------------------
+-- // Variables
+-- // ----------------------------------------------------------------------------------------------
+local playerID 						= Game.GetLocalPlayer();
+local pPlayer 						= Players[playerID];
+local pTreasury 					= pPlayer:GetTreasury();
+local pReligion 					= pPlayer:GetReligion();
+local pEnvoy 						= pPlayer:GetInfluence();
+local pVis 							= PlayersVisibility[playerID];
+local pNewGP 						= 1;
+local pNewEnvoy 					= 5;
+local pNewReligion 					= 1000;
+
+local MINIMAP_BUTTON_ID				= "CheatMenuButton";
+local m_hideCheatPanel				= false;
+local m_IsLoading:boolean			= false;
+local m_IsAttached:boolean			= false;
+
+
+-- // ----------------------------------------------------------------------------------------------
+-- // Functions
+-- // ----------------------------------------------------------------------------------------------
+function OnRegisterMinimapBarAdditions()
+  local buttonInfo = {
+    Icon = "ICON_VICTORY_DEFAULT",
+    Tooltip = "LOC_CHEAT_TOGGLE_MENU";
+    Id = MINIMAP_BUTTON_ID;
+  };
+  LuaEvents.MinimapBar_AddButton(buttonInfo);
+end
+
+function OnMinimapBarCustomButtonClicked(id:string)
+	if id == MINIMAP_BUTTON_ID then
+		OnMenuButtonToggle();
+	end
+end
+
+-- // ----------------------------------------------------------------------------------------------
+-- // MENU BUTTON FUNCTIONS
+-- // ----------------------------------------------------------------------------------------------
+function ChangeEraScore()
+	if pPlayer:IsHuman() then
+        LuaEvents.ChangePlayerScoreEra(playerID)
+    end
+	Refresh();
+end
+function ChangeGold()
+	local pNewGold = 1000
+	if pPlayer:IsHuman() then
+        LuaEvents.ChangePlayerGold(playerID, pNewGold)
+    end
+end
+function ChangeGoldMore()
+	local pNewGold = 100000;
+	if pPlayer:IsHuman() then
+        LuaEvents.ChangePlayerGold(playerID, pNewGold)
+    end
+end
+function CompleteProduction()
+	if pPlayer:IsHuman() then
+		LuaEvents.CompletePlayerProduction(playerID)
+	end
+end
+function CompleteAllResearch()
+ 	local pTechs = pPlayer:GetTechs()
+	if pPlayer:IsHuman() then		
+		LuaEvents.CompleteAllPlayerResearch(playerID)	
+	end		
+end
+function CompleteAllCivic()
+ 	local pTechs = pPlayer:GetCulture()
+	if pPlayer:IsHuman() then		
+		LuaEvents.CompleteAllPlayerCivic(playerID)	
+	end		
+end
+function CompleteResearch()
+ 	local pTechs = pPlayer:GetTechs()
+	local pRTech = pTechs:GetResearchingTech()	
+	if pRTech >= 0 then
+		local pCost = pTechs:GetResearchCost(pRTech)	
+		local pProgress = pTechs:GetResearchProgress(pRTech)
+		local pResearchComplete = (pCost - pProgress)
+		if pPlayer:IsHuman() then		
+			LuaEvents.CompletePlayerResearch(playerID, pResearchComplete)	
+		end		
+	end
+end
+function CompleteCivic()
+ 	local pCivics = pPlayer:GetCulture()
+	local pRCivic = pCivics:GetProgressingCivic()
+	if pRCivic >= 0 then		
+		local pCost = pCivics:GetCultureCost(pRCivic)	
+		local pProgress = pCivics:GetCulturalProgress(pRCivic)
+		local pCivicComplete = (pCost - pProgress)
+		if pPlayer:IsHuman() then		
+			LuaEvents.CompletePlayerCivic(playerID, pCivicComplete)	
+		end
+	end	
+end
+function ChangeFaith()
+	if pPlayer:IsHuman() then
+        LuaEvents.ChangePlayerFaith(playerID, pNewReligion)
+    end
+end
+function ChangePopulation()
+ 	if pPlayer:IsHuman() then		
+		LuaEvents.ChangePlayerPopulation(playerID)
+	end
+end
+function ChangeCityLoyalty()
+ 	if pPlayer:IsHuman() then		
+		LuaEvents.ChangePlayerLoyalty(playerID)	
+	end
+end
+function DestroyCity()
+	local pCity = UI.GetHeadSelectedCity();
+	if pPlayer:IsHuman(pCity == nil) then
+		local pCityName:string = Locale.Lookup(pCity:GetName());
+		local pCityPop:string = Locale.Lookup(pCity:GetPopulation());
+		LuaEvents.ShowRazeCityReminderPopup(playerID);
+	end
+end
+function UnitPromote()
+	local pUnit = UI.GetHeadSelectedUnit();
+    if pUnit ~= nil and pPlayer:IsHuman() then
+		local unitID = pUnit:GetID();
+        LuaEvents.ChangePromotion(playerID, unitID)
+		UI:DeselectUnitID(unitID);
+		UI:SelectUnitID(unitID);
+	end
+end
+function UnitMovementChange()
+	local pUnit = UI.GetHeadSelectedUnit();
+    if pUnit ~= nil and pPlayer:IsHuman() then
+		local unitID = pUnit:GetID();
+        LuaEvents.ChangeUnitMovement(playerID, unitID)
+		UI:DeselectUnitID(unitID);
+		UI:SelectUnitID(unitID);
+	end
+end
+function UnitAddMovement()
+	local pUnit = UI.GetHeadSelectedUnit();
+    if pUnit ~= nil and pPlayer:IsHuman() then
+		local unitID = pUnit:GetID();
+        LuaEvents.AddUnitMovement(playerID, unitID)
+		UI:DeselectUnitID(unitID);
+		UI:SelectUnitID(unitID);
+    end
+end
+function OnDuplicate()
+	local pUnit = UI.GetHeadSelectedUnit();
+	if pUnit ~= nil and pPlayer:IsHuman() then
+		local unitID = pUnit:GetID();
+		local unitType:string = GameInfo.Units[pUnit:GetUnitType()].UnitType;
+		LuaEvents.DuplicateUnit(playerID, unitID, unitType)
+    else end
+end
+function UnitHealChange()
+	local pUnit = UI.GetHeadSelectedUnit();
+    if pUnit ~= nil and pPlayer:IsHuman() then
+		local unitID = pUnit:GetID();
+        LuaEvents.ChangeUnitHealth(playerID, unitID)
+    	UI:DeselectUnitID(unitID);
+		UI:SelectUnitID(unitID);
+	end
+end
+function ChangeEnvoy()
+	if pPlayer:IsHuman() then
+        LuaEvents.ChangePlayerEnvoy(playerID, pNewEnvoy)
+    else end
+end
+function ChangeGovPoints()
+	if pPlayer:IsHuman() then
+        LuaEvents.ChangePlayerGpoints(playerID, pNewGP)
+    end
+end
+function RevealAll()
+	if pPlayer:IsHuman() then		
+		LuaEvents.ChangeFOW(playerID)	
+	end		
+end
+
+-- // ----------------------------------------------------------------------------------------------
+-- // REFRESH
+-- // ----------------------------------------------------------------------------------------------
+function Refresh()
+	if pPlayer:IsHuman() then
+		local UPContextPtr :table = ContextPtr:LookUpControl("/InGame/ActionPanel");
+		if UPContextPtr ~= nil then
+			UPContextPtr:RequestRefresh(); 
+		end
+	end
+	ContextPtr:RequestRefresh(); 
+end
+
+-- // ----------------------------------------------------------------------------------------------
+-- // HOTKEYS
+-- // ----------------------------------------------------------------------------------------------
+function OnInputActionTriggered( actionId )
+	if ( actionId == Input.GetActionId("ToggleGold") ) then
+		ChangeGold();
+	end
+	if ( actionId == Input.GetActionId("ToggleGoldMore") ) then
+		ChangeGoldMore()
+	end
+	if ( actionId == Input.GetActionId("ToggleFaith") ) then
+		ChangeFaith();
+	end
+	if ( actionId == Input.GetActionId("ToggleCProduction") ) then
+		CompleteProduction();
+	end
+	if ( actionId == Input.GetActionId("ToggleCCivic") ) then
+		CompleteCivic();
+	end
+	if ( actionId == Input.GetActionId("ToggleCResearch") ) then
+		CompleteResearch();
+	end
+	if ( actionId == Input.GetActionId("ToggleEnvoy") ) then
+		ChangeEnvoy();
+	end
+	if ( actionId == Input.GetActionId("ToggleEra") ) then
+		ChangeEraScore();
+	end
+	if ( actionId == Input.GetActionId("ToggleObs") ) then
+		RevealAll();
+	end
+	if ( actionId == Input.GetActionId("ToggleUnitMovementChange") ) then
+		UnitMovementChange();
+	end
+	if ( actionId == Input.GetActionId("ToggleUnitHealChange") ) then
+		UnitHealChange();
+	end
+	if ( actionId == Input.GetActionId("ToggleUnitPromote") ) then
+		UnitPromote();
+	end
+	if ( actionId == Input.GetActionId("ToggleDuplicate") ) then
+		OnDuplicate();
+	end
+	if ( actionId == Input.GetActionId("ToggleChangePopulation") ) then
+		ChangePopulation();
+	end
+	if ( actionId == Input.GetActionId("ToggleChangeCityLoyalty") ) then
+		ChangeCityLoyalty();
+	end
+	if ( actionId == Input.GetActionId("ToggleCompleteAllResearch") ) then
+		CompleteAllResearch();
+	end
+	if ( actionId == Input.GetActionId("ToggleCompleteAllCivic") ) then
+		CompleteAllCivic();
+	end
+	if ( actionId == Input.GetActionId("ToggleMenu") ) then
+		OnMenuButtonToggle();
+	end
+end
---
+++ UI/Panels/Cheat_Panel_World_Tracker.lua
@@ -0,0 +1,112 @@
+-- // ----------------------------------------------------------------------------------------------
+-- // Author: Sparrow
+-- // DateCreated: 01/24/2019 2:27:04 PM
+-- // ----------------------------------------------------------------------------------------------
+include("Cheat_Menu_Panel_Functions");
+
+-- // ----------------------------------------------------------------------------------------------
+-- // Functions
+-- // ----------------------------------------------------------------------------------------------
+local m_CheatPanelState:number		= 0;
+
+function AttachPanelToWorldTracker()
+	if (m_IsLoading) then
+		return;
+	end
+	if (not m_IsAttached) then
+		local worldTrackerPanel:table = ContextPtr:LookUpControl("/InGame/WorldTracker/PanelStack");
+		if (worldTrackerPanel ~= nil) then
+			Controls.CheatPanel:ChangeParent(worldTrackerPanel);
+			worldTrackerPanel:AddChildAtIndex(Controls.CheatPanel, 1);
+			worldTrackerPanel:CalculateSize();
+			worldTrackerPanel:ReprocessAnchoring();
+			m_IsAttached = true;
+		end
+	end
+end
+-- // ----------------------------------------------------------------------------------------------
+-- // Attach Panel To WorldTracker
+-- // ----------------------------------------------------------------------------------------------
+function OnLoadGameViewStateDone()
+	AttachPanelToWorldTracker();
+end
+-- // ----------------------------------------------------------------------------------------------
+-- // Panel Control and Checkbox Attach
+-- // ----------------------------------------------------------------------------------------------
+function UpdateCheatPanel(hideCheatPanel:boolean)
+	m_hideCheatPanel = hideCheatPanel; 
+	Controls.CheatPanel:SetHide(m_hideCheatPanel);
+	Controls.ToggleCheatPanel:SetCheck(not m_hideCheatPanel);
+end
+function InitDropdown()
+	local parent = ContextPtr:LookUpControl("/InGame/WorldTracker/CivicsCheckButton",	Controls.PanelStack );
+	if parent == nil then return end;
+	Controls.CheatPanelStack:ChangeParent(parent);
+	parent.ReprocessAnchoring();
+	Events.LoadGameViewStateDone.Remove(InitDropdown);
+end
+function OnPanelTitleClicked()
+    if(m_CheatPanelState == 0) then
+		UI.PlaySound("Tech_Tray_Slide_Open");
+		Controls.CheatPanel:SetSizeY(125);
+		Controls.ButtonStackMIN:SetHide(false);
+		Controls.ButtonStackADV:SetHide(false);
+		Controls.ButtonSep:SetHide(false);
+		m_CheatPanelState = 1;
+
+	else
+		UI.PlaySound("Tech_Tray_Slide_Closed");
+		Controls.CheatPanel:SetSizeY(25);
+		Controls.ButtonStackMIN:SetHide(true);
+		Controls.ButtonStackADV:SetHide(true);
+		Controls.ButtonSep:SetHide(true);
+		m_CheatPanelState = 0;
+	end	
+end
+local function InitializeControls()
+	Controls.HeaderTitle:RegisterCallback(Mouse.eLClick, OnPanelTitleClicked);
+	Controls.CheatButtonGold:RegisterCallback(Mouse.eLClick, ChangeGold);
+	Controls.CheatButtonFaith:RegisterCallback(Mouse.eLClick, ChangeFaith);
+	Controls.CheatButtonProduction:RegisterCallback(Mouse.eLClick, CompleteProduction);
+	Controls.CheatButtonScience:RegisterCallback(Mouse.eLClick, CompleteResearch);
+	Controls.CheatButtonCulture:RegisterCallback(Mouse.eLClick, CompleteCivic);
+	Controls.CheatButtonEnvoy:RegisterCallback(Mouse.eLClick, ChangeEnvoy);
+	Controls.CheatButtonAddMovement:RegisterCallback(Mouse.eLClick, UnitAddMovement);
+	Controls.CheatButtonPromote:RegisterCallback(Mouse.eLClick, UnitPromote);
+	Controls.CheatButtonHeal:RegisterCallback(Mouse.eLClick, UnitHealChange);
+	Controls.CheatButtonPopulation:RegisterCallback(Mouse.eLClick, ChangePopulation);
+	Controls.CheatButtonEra:RegisterCallback(Mouse.eLClick, ChangeEraScore);
+	Controls.CheatButtonEra:RegisterCallback( Mouse.eMouseEnter, function() UI.PlaySound("Main_Menu_Mouse_Over") end);
+	Controls.CheatButtonGold:RegisterCallback( Mouse.eMouseEnter, function() UI.PlaySound("Main_Menu_Mouse_Over") end);
+	Controls.CheatButtonProduction:RegisterCallback( Mouse.eMouseEnter, function() UI.PlaySound("Main_Menu_Mouse_Over") end);
+	Controls.CheatButtonScience:RegisterCallback( Mouse.eMouseEnter, function() UI.PlaySound("Main_Menu_Mouse_Over") end);
+	Controls.CheatButtonCulture:RegisterCallback( Mouse.eMouseEnter, function() UI.PlaySound("Main_Menu_Mouse_Over") end);
+	Controls.CheatButtonFaith:RegisterCallback( Mouse.eMouseEnter, function() UI.PlaySound("Main_Menu_Mouse_Over") end);
+	Controls.CheatButtonPopulation:RegisterCallback( Mouse.eMouseEnter, function() UI.PlaySound("Main_Menu_Mouse_Over") end);
+	Controls.CheatButtonPromote:RegisterCallback( Mouse.eMouseEnter, function() UI.PlaySound("Main_Menu_Mouse_Over") end);
+	Controls.CheatButtonAddMovement:RegisterCallback( Mouse.eMouseEnter, function() UI.PlaySound("Main_Menu_Mouse_Over") end);
+	Controls.CheatButtonHeal:RegisterCallback( Mouse.eMouseEnter, function() UI.PlaySound("Main_Menu_Mouse_Over") end);
+	Controls.CheatButtonEnvoy:RegisterCallback( Mouse.eMouseEnter, function() UI.PlaySound("Main_Menu_Mouse_Over") end);
+	Controls.ToggleCheatPanel:RegisterCheckHandler(function() UpdateCheatPanel(not m_hideCheatPanel); end);
+	Controls.ToggleCheatPanel:SetCheck(true);
+	UpdateCheatPanel(true);
+end
+-- // ----------------------------------------------------------------------------------------------
+-- // Init
+-- // ----------------------------------------------------------------------------------------------
+function Initialize()
+	m_IsLoading = true;
+
+		Events.LoadGameViewStateDone.Add(OnLoadGameViewStateDone);
+		Events.LoadGameViewStateDone.Add(InitDropdown);
+		InitializeControls();
+		if(GameConfiguration.IsNetworkMultiplayer()) then
+			UpdateCheatPanel(true);
+			Controls.ToggleCheatPanel:SetHide(true);
+		else
+			UpdateCheatPanel(false);
+		end
+
+	m_IsLoading = false;
+end
+Initialize();
